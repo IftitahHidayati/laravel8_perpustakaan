4a090b69a5e9ded7736042f5b8605a0c2229481e
IftitahHidayati <hidayatiiftitah@gmail.com>
2022-12-21 20:13:53 +0700

    tes

--- app/Http/Controllers/Peminjam/BukuController.php
+++ app/Http/Controllers/Peminjam/BukuController.php
@@ -17,42 +17,4 @@ class BukuController extends Controller
     {
         return view('peminjam/buku/index');
     }
-
-    if ($request->file('image')) {
-        $image_name = $request->file('image');
-        // $image_name = $request->file('image')->store('images', 'public');
-        $storage = new StorageClient([
-            'keyFilePath' => public_path('key.json')
-        ]);
-
-        $bucketName = env('GOOGLE_CLOUD_BUCKET');
-        $bucket = $storage->bucket($bucketName);
-
-        //get filename with extension
-        $filenamewithextension = pathinfo($request->file('image')->getClientOriginalName(), PATHINFO_FILENAME);
-        // $filenamewithextension = $request->file('image')->getClientOriginalName();
-
-        //get filename without extension
-        $filename = pathinfo($filenamewithextension, PATHINFO_FILENAME);
-
-        //get file extension
-        $extension = $request->file('image')->getClientOriginalExtension();
-
-        //filename to store
-        $filenametostore = $filename . '_' . uniqid() . '.' . $extension;
-
-        Storage::put('public/uploads/' . $filenametostore, fopen($request->file('image'), 'r+'));
-
-        $filepath = storage_path('app/public/uploads/' . $filenametostore);
-
-        $object = $bucket->upload(
-            fopen($filepath, 'r'),
-            [
-                'predefinedAcl' => 'publicRead'
-            ]
-        );
-
-        // delete file from local disk
-        Storage::delete('public/uploads/' . $filenametostore);
-    }
 }
